4e20afe905823788ab0c97a615435e68c350a7b8
Filippo Federico <filippofederico@mbp-filippo.intern>
2019-06-21 16:21:36 +0200

    imported logo from new folder

--- src/Components/Logo.js
+++ src/Components/Logo.js
@@ -2,14 +2,15 @@
 
 
 import React from 'react';
-
+import logo from '../initiallandingpagedesign/MYtineraryLogo.png'
+console.log(logo)
 class Logo extends React.Component {
     
   
     render() {
       return (
         <div>
-          <h1>Logo</h1>
+          <img src={logo} alt="Logo" />
         </div>
       );
     }
