ded377708975e9a2e78fac26794bf797d4d5068f
Filippo Federico <filippofederico@mbp-filippo.intern>
2019-06-26 15:45:19 +0200

    slider/carousel

--- src/Components/Content.js
+++ src/Components/Content.js
@@ -1,27 +1,46 @@
 import React from 'react'
 import SimpleImageSlider from "react-simple-image-slider";
-import imgSlider from '../imgSlider.js/barcelona.jpg'
+
+import barcelona from '../imgSlider.js/barcelona.jpg'
+import hollywood from '../imgSlider.js/hollywood.jpg'
+import london from '../imgSlider.js/london.jpg'
+import miami from '../imgSlider.js/miami.jpg'
+import milano from '../imgSlider.js/milano.jpg'
+import moscow from '../imgSlider.js/moscow.jpg'
+import newyork from '../imgSlider.js/newyork.jpg'
+import riodejaneiro from '../imgSlider.js/riodejaneiro.jpg'
+import roma from '../imgSlider.js/roma.jpg'
+import shanghai from '../imgSlider.js/shanghai.jpg'
+import sidney from '../imgSlider.js/sidney.jpg'
+import paris from '../imgSlider.js/paris.jpg'
+
+
+
+
 
 class Content extends React.Component {
     render() {
-        const images = [
-            { url: imgSlider },
-            { url: "images/2.jpg" },
-            { url: "images/3.jpg" },
-            { url: "images/4.jpg" },
-            { url: "images/5.jpg" },
-            { url: "images/6.jpg" },
-            { url: "images/7.jpg" },
-        ];
+        const images = [{url: barcelona}, {url:hollywood}, {url:london}, {url:miami}, {url:milano}, {url:moscow}, {url:newyork}, {url:paris}, {url:riodejaneiro}, {url:roma}, {url:shanghai}, {url:sidney}];
+                
+                
                 return (
-            <div>
-                <SimpleImageSlider
-                    width={'100%'}
-                    height={'200px'}
+                    <div className="containerSlider">
+                    <h3>Popular MYtineraries</h3>
+            <div className="slider">
+                
+                
+                    <SimpleImageSlider
+                    width={400}
+                    height={300}
                     images={images}
-                />
+                    />
+                
+                    </div>
             </div>
         );
     }
 }
+
+
+
 export default Content;
